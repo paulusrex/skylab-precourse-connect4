1b15dd35225707b3f6487681854eac0dd54782e8
Pablo Rey <pabloreysobral@gmail.com>
2019-01-21 17:07:03 +0100

    bugfix in winner logic animations added (needs more work to be fine)

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ class Connect4 {
     this.rows = rows;
     this.nowPlaying = 1;
     this.playerColors = ["white", "red", "blue"];
+    this.playerNames = ["", "Rojo", "Azul"];
+    this.animationInProgress = false;
     this.board = [];
     this.initBoard();
     this.drawInit();
@@ -22,16 +24,20 @@ class Connect4 {
     for (let c = 0; c < this.cols; c++) {
       $("#board").append(`<div class="col" id="col-${c}"></div>`);
       $(`#chip-selector`).append(
-        `<div class="col-anim" id="col-anim-${c}" onclick="game.onClick(${c})"></div>`
+        `<div 
+            class="col-anim" 
+            id="col-anim-${c}" 
+            onclick="game.onClick(${c});" 
+            onmouseover="game.onMouseOver(${c});"
+            onmouseout="game.onMouseOut(${c});"
+          >
+         </div>`
       );
       $(`#col-anim-${c}`).append(`<div class="anim" id="anim-${c}"></div>`);
       for (let r = this.rows - 1; r >= 0; r--) {
         $(`#col-${c}`).append(`<div class="chip" id="chip-${c}${r}"></div>`);
       }
     }
-    // $(".anim").hover(function() {
-    //   $(this).css("background-color", colorPlayer);
-    // });
   }
 
   drawBoard() {
@@ -49,59 +55,93 @@ class Connect4 {
     });
   }
 
+  isColumnFull(col) {
+    return this.board[col][this.rows - 1] !== false;
+  }
+
   dropChip(col, player) {
-    if (this.board[col][this.rows - 1]) {
+    if (this.isColumnFull(col)) {
       // column full
       return false;
     }
     const firstEmpty = this.board[col].findIndex(chip => !chip);
     this.board[col][firstEmpty] = player;
-    // $(`#chip-${col}${firstEmpty}`) // TODO: animate
+    return firstEmpty;
   }
 
-  onClick(col) {
+  async onClick(col) {
     if (!this.nowPlaying) {
       return;
     }
-    this.dropChip(col, this.nowPlaying);
-    this.drawBoard();
+    const row = this.dropChip(col, this.nowPlaying);
     const winner = this.checkWinner();
     if (winner) {
       this.nowPlaying = false;
-      alert("Ha ganado el jugador " + winner.player);
+      this.drawBoard();
+      await new Promise(resolve => setTimeout(resolve, 800));
+      alert("Ha ganado el jugador " + this.playerNames[winner.player]);
       // player: b[c][r],
       // init: { col: c, row: r },
       // type: "backslash",
     } else {
+      this.animationInProgress = true;
+      $(`#anim-${col}`)
+        .css("transition", "transform .6s")
+        .css("transform", `translate(0px,${(6 - row) * 110 - 5}px)`);
+      await new Promise(resolve => setTimeout(resolve, 800));
+      $(`#anim-${col}`)
+        .css("transition", "")
+        .css("transform", `translate(0px,0px)`);
+      this.animationInProgress = false;
+      this.drawBoard();
       this.nowPlaying = this.nowPlaying === 1 ? 2 : 1;
+      this.onMouseOver(col);
     }
+  }
+
+  onMouseOver(col) {
+    if (this.animationInProgress) {
+      return;
+    }
+    for (let c = 0; c < this.cols; c++) {
+      if (c === col) {
+        if (!this.isColumnFull(col)) {
+          $(`#anim-${col}`).css(
+            "background-color",
+            this.playerColors[this.nowPlaying]
+          );
+        }
+      } else {
+        $(`#anim-${c}`).css("background-color", "transparent");
+      }
+    }
+  }
+
+  onMouseOut(col) {
+    if (this.animationInProgress) {
+      return;
+    }
+    $(`#anim-${col}`).css("background-color", "transparent");
   }
 
   checkWinner() {
     const b = this.board;
+    const checkAux = [1, 2, 3];
     const checkX = (col, row) =>
       b[col][row] !== false &&
-      b[col][row] === b[col + 1][row] &&
-      b[col][row] === b[col + 2][row] &&
-      b[col][row] === b[col + 3][row];
+      checkAux.every(diff => b[col][row] === b[col + diff][row]);
     const checkY = (col, row) =>
       b[col][row] !== false &&
-      b[col][row] === b[col][row + 1] &&
-      b[col][row] === b[col][row + 2] &&
-      b[col][row] === b[col][row + 3];
+      checkAux.every(diff => b[col][row] === b[col][row + diff]);
     const checkForwardSlash = (col, row) =>
       b[col][row] !== false &&
-      b[col][row] === b[col + 1][row + 1] &&
-      b[col][row] === b[col + 2][row + 2] &&
-      b[col][row] === b[col + 3][row + 3];
+      checkAux.every(diff => b[col][row] === b[col + diff][row + diff]);
     const checkBackSlash = (col, row) =>
       b[col][row] !== false &&
-      b[col][row] === b[col + 1][row - 1] &&
-      b[col][row] === b[col + 2][row - 2] &&
-      b[col][row] === b[col + 3][row - 3];
+      checkAux.every(diff => b[col][row] === b[col + diff][row - diff]);
 
     // Check horizontally
-    for (let c = 0; c < this.cols - 4; c++) {
+    for (let c = 0; c <= this.cols - 4; c++) {
       for (let r = 0; r < this.rows; r++) {
         if (checkX(c, r)) {
           return { player: b[c][r], init: { col: c, row: r }, type: "X" };
@@ -111,7 +151,7 @@ class Connect4 {
 
     // check vertically
     for (let c = 0; c < this.cols; c++) {
-      for (let r = 0; r < this.rows - 4; r++) {
+      for (let r = 0; r <= this.rows - 4; r++) {
         if (checkY(c, r)) {
           return { player: b[c][r], init: { col: c, row: r }, type: "Y" };
         }
@@ -119,8 +159,8 @@ class Connect4 {
     }
 
     // check diagonal '/'
-    for (let c = 0; c < this.cols - 4; c++) {
-      for (let r = 0; r < this.rows - 4; r++) {
+    for (let c = 0; c <= this.cols - 4; c++) {
+      for (let r = 0; r <= this.rows - 4; r++) {
         if (checkForwardSlash(c, r)) {
           return {
             player: b[c][r],
@@ -132,7 +172,7 @@ class Connect4 {
     }
 
     // check diagonal '\'
-    for (let c = 0; c < this.cols - 4; c++) {
+    for (let c = 0; c <= this.cols - 4; c++) {
       for (let r = this.rows - 4; r < this.rows; r++) {
         if (checkBackSlash(c, r)) {
           return {
